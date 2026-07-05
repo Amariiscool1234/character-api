d3d6c367ab95395512d8d52cd0988af15cda78d1
Amari <amariames443@gmail.com>
2026-07-05 19:15:48 -0400

    initial spring boot setup

---
+++ src/test/java/com/amari/character_api/CharacterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amari.character_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharacterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

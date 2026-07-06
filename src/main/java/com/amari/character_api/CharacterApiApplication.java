f746a4bf6af4a32ae53b248276b304cc19f1e89f
Amari <amariames443@gmail.com>
2026-07-06 13:16:38 -0400

    fix the modified file

--- src/main/java/com/amari/character_api/CharacterApiApplication.java
+++ src/main/java/com/amari/character_api/CharacterApiApplication.java
@@ -3,7 +3,7 @@ package com.amari.character_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.amari.character_api")
 public class CharacterApiApplication {
 
 	public static void main(String[] args) {

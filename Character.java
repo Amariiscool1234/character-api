73b11b6f6c0b07c2892b162da700b840e4528fbc
Amari <amariames443@gmail.com>
2026-07-05 23:55:11 -0400

    add the modified java file

--- Character.java
+++ Character.java
@@ -15,8 +15,8 @@ public class Character {
 @Column(nullable = false)
  private String name;
 
-  @Column(nullable = false)
-  private String descripton;
+   @Column(nullable = false)
+   private String description;
 
   private String universe;
 
@@ -25,15 +25,13 @@ public class Character {
   public Character() {}
 
 
-  public Character(Long characterId, String name, String description, String universe, String species) {
-
-     this.characterId = characterId;
-     this.name = name;
+   public Character(Long characterId, String name, String description, String universe, String species) {
+      this.characterId = characterId;
+      this.name = name;
       this.description = description;
-     this.universe = universe;
-     this.species = species;
-
-  }
+      this.universe = universe;
+      this.species = species;
+   }
 
    public Long getCharacterId() {
      return characterId;
@@ -43,7 +41,7 @@ public class Character {
         this.characterId = characterId;
      }
 
-     public String getname() {
+     public String getName() {
         return name;
      }
 
@@ -57,6 +55,10 @@ public class Character {
 
      public void setDescription(String description) {
         this.description = description;
+     }
+
+     public void setUniverse(String universe) {
+        this.universe = universe;
      }
 
      public String getUniverse() {

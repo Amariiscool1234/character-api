38aebe0152c222715a9f5e7b1d842df7a42b7d79
Amari <amariames443@gmail.com>
2026-07-06 13:07:45 -0400

    fix the modified and untracked files

--- src/main/java/com/amari/character_api/CharacterRepository.java
+++ src/main/java/com/amari/character_api/CharacterRepository.java
@@ -1,8 +1,10 @@
 package com.amari.character_api;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
+@Repository
 public interface CharacterRepository extends JpaRepository<Character, Long>{
     List<Character> findByNameContaining(String name);
 

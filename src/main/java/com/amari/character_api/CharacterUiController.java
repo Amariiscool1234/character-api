b9704c7f3b30e9dcba0e1a95b1338ef2bdc23c36
Amari <amariames443@gmail.com>
2026-07-13 22:00:20 -0400

    fix all the modified and untracked files

--- src/main/java/com/amari/character_api/CharacterUiController.java
+++ src/main/java/com/amari/character_api/CharacterUiController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CharacterUiController {
@@ -32,5 +33,41 @@ public class CharacterUiController {
 
         model.addAttribute("character", character);
         return "character-details";
+
     }
+
+     @GetMapping("/characters/create")
+      public String showCreateForm() {
+        return "character-create";
+      }
+
+      @PostMapping("/characters/create")
+       public String createCharacter(Character character) {
+         Character savedCharacter = characterService.createCharacter(character);
+          return "redirect:/characters/" + savedCharacter.getCharacterId();
+       }
+    
+        @GetMapping("/characters/{id}/edit")
+    public String showUpdateForm(@PathVariable Long id, Model model) {
+        Character character = characterService.getCharacterById(id)
+                .orElseThrow(() -> new RuntimeException("Character was not found"));
+
+        model.addAttribute("character", character);
+        return "character-update";
+    }
+
+    @PostMapping("/characters/{id}/edit")
+    public String updateCharacter(@PathVariable Long id, Character character) {
+        characterService.updateCharacter(id, character);
+        return "redirect:/characters/" + id;
+    }
+
+     @GetMapping("/characters/{id}/delete")
+    public String deleteCharacter(@PathVariable Long id) {
+        characterService.deleteCharacter(id);
+        return "redirect:/characters";
+
+}
+
+
 }
